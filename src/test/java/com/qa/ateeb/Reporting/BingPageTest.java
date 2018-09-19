f0289306da443c55cedbd1116bb9797bf760d0e5
ateeb32 <l.ateeb@hotmail.com>
2018-09-19 10:27:23 +0100

    repooo

--- src/test/java/com/qa/ateeb/Reporting/BingPageTest.java
+++ src/test/java/com/qa/ateeb/Reporting/BingPageTest.java
@@ -21,13 +21,13 @@ public class BingPageTest {
 
 	static WebDriver driver = null;
 	
-	public static ExtentReports report;
-	public ExtentTest test;
+	public static ExtentReports report; //REPORTING
+	public ExtentTest test; //REPORTING
 	
 	@BeforeClass
 	public static void initial() {
 		
-		report = new ExtentReports("C:\\Users\\Admin\\Desktop\\My Eclipse Workspace\\Reporting\\Reports\\Report.html", true);
+		report = new ExtentReports("C:\\Users\\Admin\\Desktop\\My Eclipse Workspace\\Reporting\\Reports\\Report.html", true); //REPORTING
 		
 	}
 	
@@ -43,7 +43,7 @@ public class BingPageTest {
 	public void bingSearchBox() throws InterruptedException {
 		test = report.startTest("Search Text");
 		driver.get("https://www.bing.com/");
-		test.log(LogStatus.INFO, "Bing Opened");
+		test.log(LogStatus.INFO, "Bing Opened"); //REPORTING
 		BingPage page = PageFactory.initElements(driver, BingPage.class); // Call all elements from BingPage class
 		BingSearchPage searchPage = PageFactory.initElements(driver, BingSearchPage.class);
 		page.searchFor("Selenium");
@@ -51,15 +51,15 @@ public class BingPageTest {
 		//Thread.sleep(1000);
 		//WebElement checkElement = driver.findElement(By.xpath("//*[@id=\"b_context\"]/li[1]/div/div[1]/h2"));
 		
-		test.log(LogStatus.INFO, "Search RUN");
-		if (searchPage.getHeading().getText().equals("Selenium")) {
+		test.log(LogStatus.INFO, "Search RUN"); //REPORTING
+		if (searchPage.getHeading().getText().equals("Selenium")) { //REPORTING
 			
 			test.log(LogStatus.PASS, "Search Text Was Found");
 			System.out.println("true");
 			
 		}
 		
-		else {
+		else { //REPORTING
 			
 			test.log(LogStatus.FAIL, "Search Text Was NOT Found");
 			System.out.println("false");
@@ -67,7 +67,7 @@ public class BingPageTest {
 		}
 		
 		assertEquals("Selenium", searchPage.getHeading().getText());
-		report.endTest(test);
+		report.endTest(test); //REPORTING
 		
 		//assertEquals("Selenium");
 		
@@ -115,7 +115,7 @@ public class BingPageTest {
 	}
 	
 	@AfterClass
-	public static void end() {
+	public static void end() { //REPORTING
 		
 		report.flush();
 		
